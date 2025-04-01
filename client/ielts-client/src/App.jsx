e2220f84d2244c6486358af05266b469f54a1146
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-04-01 18:51:01 -0400

    Initial commit

---
+++ client/ielts-client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl text-center mt-10 text-blue-600">
+      ✅ Tailwind CSS is working!
+    </div>
+  );
+}
+
+export default App;
